d3aadf11338c4d8085e5f90328c0ca6a5c4c2895
Jack Somerset <jack.somerset@mobilise.cloud>
2021-03-08 16:46:12 +0000

    Completed vpc_basic module

---
+++ interface.tf
@@ -0,0 +1,39 @@
+variable "name" {
+     type        = string
+     description = "The name of the VPC."
+}
+
+variable "cidr" {
+     type = string
+     description = "The CIDR of the VPC."
+}
+
+variable "public_subnet" {
+     type        = string
+     description = "The public subnet to create."
+}
+
+variable "enable_dns_hostnames" {
+     type = bool
+     description = "Should be true if you want to use private DNS within the VPC"
+     default     = true
+}
+
+variable "enable_dns_support" {
+     type        = bool
+     description = "Should be true if you want to use private DNS within the VPC"
+     default     = true
+}
+
+# Outputs section
+output "public_subnet_id" {
+     value = aws_subnet.public.id
+}
+
+output "vpc_id" {
+     value = aws_vpc.tfb.id
+}
+
+output "cidr" {
+     value = aws_vpc.tfb.cidr_block
+}   
